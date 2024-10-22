84ca7080457f8bfbeee50fbe552dc79dc430f0e2
shadowhexer <austinmanlangit@gmail.com>
2024-10-22 17:23:42 +0800

    Updated syntax

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'vutify/styles'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
