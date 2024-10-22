0506e3c8af70a45302539b07697d544f3a1745c5
shadowhexer <austinmanlangit@gmail.com>
2024-10-22 18:02:21 +0800

    Test

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
