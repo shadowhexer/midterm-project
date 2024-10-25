a3e713aa7be1a3f769b4032505322112871a116a
shadowhexer <austinmanlangit@gmail.com>
2024-10-25 11:31:39 +0800

    Initial changes.

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@ import HelloWorld from './components/LoginDesktop.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld />
 
       <nav>
       </nav>
@@ -18,66 +17,61 @@ import HelloWorld from './components/LoginDesktop.vue'
   <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+* {
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  background: #f6f5f7;
+  color: #333 !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Montserrat', sans-serif;
+  height: 100vh;
+  margin: -20px 0 50px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+h1 {
+  font-weight: bold;
+  margin: 0;
+}
+
+h2 {
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+p {
+  font-size: 14px;
+  font-weight: 100;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  margin: 20px 0 30px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+span {
+  font-size: 12px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+a {
+  color: #333;
+  font-size: 14px;
+  text-decoration: none;
+  margin: 15px 0;
 }
 
-nav a:first-of-type {
-  border: 0;
+footer p {
+  margin: 10px 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+footer i {
+  color: red;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+footer a {
+  color: #3c97bf;
+  text-decoration: none;
 }
 </style>
