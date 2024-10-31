843ea24a740794fe3dadfc3cb540698cc3cfd30f
shadowhexer <austinmanlangit@gmail.com>
2024-10-31 21:55:57 +0800

    Error fixing

--- src/types/@jamescoyle_vue-icon.d.ts
+++
@@ -1,7 +0,0 @@
-// src/types/@jamescoyle_vue-icon.d.ts
-declare module '@jamescoyle/vue-icon' {
-    import { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-  }
-  
---
+++ src/types/modules.d.ts
@@ -0,0 +1,5 @@
+// src/shims-vue.d.ts
+declare module '*.vue';
+declare module '@jamescoyle/vue-icon';
+
+  
